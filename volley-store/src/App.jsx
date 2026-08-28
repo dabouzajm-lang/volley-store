7010692d53d9d657ab4927efcf79963733a972f0
dabouzajm-lang <dabouza.j.m@gmail.com>
2026-08-28 10:30:38 -0300

    Refactor routing to use HashRouter and clean up component exports

--- volley-store/src/App.jsx
+++ volley-store/src/App.jsx
@@ -1,24 +1,33 @@
 import {
-    BrowserRouter,
+    HashRouter,
     Routes,
     Route
 } from "react-router-dom";
 
 import Navbar from "./components/NavBar/Navbar";
+
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+
 import Footer from "./components/Footer/Footer";
+
 import Home from "./components/Home/Home";
+
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+
 import Cart from "./components/Cart/Cart";
+
 import Checkout from "./components/Checkout/Checkout";
+
 import NotFound from "./components/NotFound/NotFound";
+
 
 function App() {
 
     return (
 
-        <BrowserRouter>
+        <HashRouter>
 
             <Navbar />
 
@@ -31,30 +40,36 @@ function App() {
                     element={<Home />}
                 />
 
+
                 <Route
                     path="/productos"
                     element={<ItemListContainer />}
                 />
+
 
                 <Route
                     path="/categoria/:categoryId"
                     element={<ItemListContainer />}
                 />
 
+
                 <Route
                     path="/item/:itemId"
                     element={<ItemDetailContainer />}
                 />
+
 
                 <Route
                     path="/cart"
                     element={<Cart />}
                 />
 
+
                 <Route
                     path="/checkout"
                     element={<Checkout />}
                 />
+
 
                 <Route
                     path="*"
@@ -65,10 +80,11 @@ function App() {
 
             <Footer />
 
-        </BrowserRouter>
+        </HashRouter>
 
     );
 
 }
+
 
 export default App;
